41c89d8b7c5a8175a7341b7b7599bbfff7c0d158
amit928 <61894594+amit928@users.noreply.github.com>
2020-12-05 21:50:18 +0530

    Add files via upload

---
+++ csvfileproject.py
@@ -0,0 +1,12 @@
+import csv
+file=open('csvfile2.csv','r')
+read=csv.reader(file)
+x=input("Enter your Event ID : ")
+for row in read:
+    if(row[3]==x):
+        print(row)
+    else:
+        print("Enter a valid Event ID")
+        break
+
+    
